49bb479222a04b972ddb882ec40d88c1290ca9fd
Paulo Suderio <paulo.suderio@gmail.com>
2024-12-11 16:44:13 -0300

    Ensure some basic languages

--- .config/nvim/lua/plugins/custom.lua
+++ .config/nvim/lua/plugins/custom.lua
@@ -160,8 +160,24 @@ return {
     opts = function(_, opts)
       -- add tsx and treesitter
       vim.list_extend(opts.ensure_installed, {
+        "bash",
+        "go",
+        "html",
+        "java",
+        "javascript",
+        "json",
+        "kotlin",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "rust",
         "tsx",
         "typescript",
+        "vim",
+        "yaml",
       })
     end,
   },
