e799a0cb089c6df9ad57ff5b15df90be421d488c
Paulo Suderio <paulo.suderio@gmail.com>
2025-03-19 15:43:12 -0300

    FiraCode & Noto in neovide

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,7 +3,7 @@
 -- Add any additional options here
 
 if vim.g.neovide then
-	vim.o.guifont = "Hack:h10:i:#e-subpixelantialias:#h-none"
+	vim.o.guifont = "FiraCode Nerd Font Mono,NotoSansM Nerd Font Mono:h10:#e-subpixelantialias:#h-none"
 	vim.g.neovide_text_gamma = 0.0
 	vim.g.neovide_text_contrast = 0.5
 	vim.g.neovide_padding_top = 0
@@ -16,6 +16,5 @@ if vim.g.neovide then
 	vim.g.neovide_floating_z_height = 10
 	vim.g.neovide_light_angle_degrees = 45
 	vim.g.neovide_light_radius = 5
-
 	vim.g.neovide_floating_corner_radius = 0.0
 end
