6fe6ab7dd40a0435ea52a478a4cbf8492912677e
Paulo Suderio <paulo.suderio@gmail.com>
2025-04-08 17:27:06 -0300

    doom config

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-;; (setq user-full-name "John Doe"
-;;       user-mail-address "john@doe.com")
+(setq user-full-name "Paulo Suderio"
+       user-mail-address "paulo.suderio@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
@@ -21,8 +21,14 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+(defun font-installed? (font-name)
+  "Retorna t se a fonte font-name está instalada"
+  (if (find-font (font-spec :name font-name))
+      t nil))
+
+(if (and (font-installed? "FiraCode Nerd Font") (font-installed? "FiraCode Nerd Font Mono"))
+(setq doom-font (font-spec :family "FiraCode Nerd Font" :size 12 :weight 'semi-light)
+      doom-variable-pitch-font (font-spec :family "FiraCode Nerd Font Mono" :size 13)))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
@@ -32,15 +38,15 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-vibrant)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
+(setq display-line-numbers-type 'relative)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq org-directory "~/Org/")
 
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
