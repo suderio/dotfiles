3c2440a5be7876c881b610a08f2cd08a652082a3
Paulo Suderio <paulo.suderio@gmail.com>
2025-04-10 00:06:21 -0300

    justfile major mode

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -24,3 +24,7 @@
   (setq magit-revision-show-gravatars '("^Author:     " . "^Commit:     ")))
 (after! magit
   (setq magit-diff-refine-hunk 'all))
+
+(use-package! justl
+  :config
+  (map! :n "e" 'justl-exec-recipe))
