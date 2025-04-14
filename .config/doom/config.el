ef609ab4fa0e62b45322442cdfaf543feaabb8cb
Paulo Suderio <paulo.suderio@gmail.com>
2025-04-13 22:53:04 -0300

    Julia emacs config

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -130,3 +130,7 @@
   (setq magit-revision-show-gravatars '("^Author:     " . "^Commit:     ")))
 (after! magit
   (setq magit-diff-refine-hunk 'all))
+
+(setq lsp-julia-package-dir nil)
+(after! lsp-julia
+  (setq lsp-julia-default-environment "~/.julia/environments/v1.11"))
---
+++ .config/julia/emacs.jl
@@ -0,0 +1,3 @@
+using Pkg
+Pkg.add("LanguageServer")
+Pkg.add("SymbolServer")
