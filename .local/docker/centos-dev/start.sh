446eefd94502186ee36f39537a2e3dbd326e5769
Paulo Suderio <paulo.suderio@gmail.com>
2025-05-06 15:31:09 -0300

    fix: docker centos start

--- .local/docker/centos-dev/start.sh
+++ .local/docker/centos-dev/start.sh
@@ -1,12 +1,7 @@
-echo $1
-
-exit 1
-
 docker build -t centos-dev .
 
 docker run -it --rm \
-  -v $1:$1 \
-  -e UID=$(id -u) -e GID=$(id -g) \
-  -e HOME=$HOME -e USERNAME=$(id -un) \
+  -v "$HOME":"/home/$(id -un)" \
+  -e UID="$(id -u)" -e GID="$(id -g)" \
+  -e HOME="/home/$(id -un)" -e USERNAME="$(id -un)" \
   centos-dev
-
