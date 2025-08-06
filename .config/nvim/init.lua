0f49d7fb45c71fe200889005eb6a30967ec4768e
Paulo Suderio <paulo.suderio@gmail.com>
2025-08-06 15:14:42 -0300

    new nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,61 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+-- Options
+vim.opt.winborder = "rounded"
+vim.opt.tabstop = 2
+vim.opt.cursorcolumn = false
+vim.opt.ignorecase = true
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.opt.wrap = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.swapfile = false
+vim.opt.termguicolors = true
+vim.opt.undofile = true
+vim.opt.incsearch = true
+vim.opt.signcolumn = "yes"
+-- Keymaps
+local map = vim.keymap.set
+
+vim.g.mapleader = " "
+map('n', '<leader>o', ':update<CR> :source<CR>')
+map('n', '<leader>w', ':write<CR>')
+map('n', '<leader>q', ':quit<CR>')
+map({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
+map({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
+map({ 'n', 'v', 'x' }, '<leader>s', ':e #<CR>')
+map({ 'n', 'v', 'x' }, '<leader>S', ':sf #<CR>')
+map('n', '<leader>f', ":Pick files<CR>")
+map('n', '<leader>h', ":Pick help<CR>")
+map('n', '<leader>e', ":Oil<CR>")
+map('n', '<leader>lf', vim.lsp.buf.format)
+
+-- Plugins
+vim.pack.add({
+	{ src = "https://github.com/vague2k/vague.nvim" },
+	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = "https://github.com/echasnovski/mini.pick" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+	{ src = "https://github.com/chomosuke/typst-preview.nvim" },
+	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = 'https://github.com/NvChad/showkeys', opt = true },
+})
+require("mason").setup({
+    ui = {
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+        }
+    }
+})
+require "showkeys".setup({ position = "top-right" })
+require "mini.pick".setup()
+require "oil".setup()
+
+vim.lsp.enable({ "bash-language-server", "lua_ls", "svelte", "tinymist", "emmetls" })
+
+
+-- themes & colors
+require "vague".setup({ transparent = true })
+vim.cmd("colorscheme vague")
+vim.cmd(":hi statusline guibg=NONE")
