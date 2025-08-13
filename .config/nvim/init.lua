ba2018feab29bdcf4da6d2fca47bdff8396d2fde
Paulo Suderio <paulo.suderio@gmail.com>
2025-08-13 17:17:16 -0300

    nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,61 +1,92 @@
--- Options
+vim.cmd([[set mouse=]])
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ','
 vim.opt.winborder = "rounded"
+vim.opt.hlsearch = false
 vim.opt.tabstop = 2
 vim.opt.cursorcolumn = false
 vim.opt.ignorecase = true
 vim.opt.shiftwidth = 2
 vim.opt.smartindent = true
-vim.opt.wrap = true
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.swapfile = false
 vim.opt.termguicolors = true
 vim.opt.undofile = true
-vim.opt.incsearch = true
 vim.opt.signcolumn = "yes"
--- Keymaps
-local map = vim.keymap.set
 
-vim.g.mapleader = " "
+local map = vim.keymap.set
 map('n', '<leader>o', ':update<CR> :source<CR>')
 map('n', '<leader>w', ':write<CR>')
 map('n', '<leader>q', ':quit<CR>')
-map({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
-map({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
-map({ 'n', 'v', 'x' }, '<leader>s', ':e #<CR>')
-map({ 'n', 'v', 'x' }, '<leader>S', ':sf #<CR>')
+map('n', '<leader>v', ':e $MYVIMRC<CR>')
+map('n', '<leader>z', ':e ~/.bashrc<CR>')
+map('n', '<leader>s', ':e #<CR>')
+map('n', '<leader>S', ':sf #<CR>')
+map({ 'n', 'v' }, '<leader>y', '"+y')
+map({ 'n', 'v' }, '<leader>d', '"+d')
+map({ 'n', 'v' }, '<leader>c', '1z=')
+
 map('n', '<leader>f', ":Pick files<CR>")
 map('n', '<leader>h', ":Pick help<CR>")
 map('n', '<leader>e', ":Oil<CR>")
 map('n', '<leader>lf', vim.lsp.buf.format)
 
--- Plugins
-vim.pack.add({
-	{ src = "https://github.com/vague2k/vague.nvim" },
+vim.pack.add {
+	{ src = "https://github.com/Olical/nfnl" },
+	{ src = 'https://github.com/neovim/nvim-lspconfig' },
+	{ src = 'https://github.com/mason-org/mason.nvim' },
+	{ src = 'https://github.com/mason-org/mason-lspconfig.nvim' },
+	{ src = 'https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim' },
+  { src = "https://github.com/vague2k/vague.nvim" },
 	{ src = "https://github.com/stevearc/oil.nvim" },
 	{ src = "https://github.com/echasnovski/mini.pick" },
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
 	{ src = "https://github.com/chomosuke/typst-preview.nvim" },
-	{ src = "https://github.com/mason-org/mason.nvim" },
 	{ src = 'https://github.com/NvChad/showkeys', opt = true },
-})
-require("mason").setup({
-    ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
-    }
+	{ src = "https://github.com/L3MON4D3/LuaSnip" },
+}
+
+require('mason-tool-installer').setup({
+	ensure_installed = {
+--		"lua_ls",
+--		"stylua",
+	}
 })
 require "showkeys".setup({ position = "top-right" })
 require "mini.pick".setup()
 require "oil".setup()
 
-vim.lsp.enable({ "bash-language-server", "cuelsp", "emmetls", "lua_ls", "svelte", "tinymist" })
+vim.lsp.config('lua_ls', {
+	settings = {
+		Lua = {
+			runtime = {
+				version = 'LuaJIT',
+			},
+			diagnostics = {
+				globals = {
+					'vim',
+					'require'
+				},
+			},
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+		},
+	},
+})
 
+require('nvim-treesitter.configs').setup({ highlight = { enable = true, }, })
 
--- themes & colors
+-- colors
 require "vague".setup({ transparent = true })
 vim.cmd("colorscheme vague")
 vim.cmd(":hi statusline guibg=NONE")
+
+-- snippets
+require("luasnip").setup({ enable_autosnippets = true })
+require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/snippets/" })
+local ls = require("luasnip")
+map("i", "<C-e>", function() ls.expand_or_jump(1) end, { silent = true })
+map({ "i", "s" }, "<C-J>", function() ls.jump(1) end, { silent = true })
+map({ "i", "s" }, "<C-K>", function() ls.jump(-1) end, { silent = true })
