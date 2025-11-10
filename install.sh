0f1d55771b968c961119efea2e2573778c2054f7
Paulo Suderio <paulo.suderio@gmail.com>
2025-11-10 18:32:52 -0300

    install script updated

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
-alias config='git --git-dir="$HOME"/.local/dotfiles/ --work-tree="$HOME"'
+git clone --bare git@github.com:suderio/dotfiles "$HOME/.local/dotfiles"
+git --git-dir="$HOME"/.local/dotfiles/ --work-tree="$HOME" config --local status.showUntrackedFiles no
+git --git-dir="$HOME"/.local/dotfiles/ --work-tree="$HOME" checkout
 
-git clone --bare git@github.com:suderio/dotfiles $HOME/.local/dotfiles
-config config --local status.showUntrackedFiles no
-config checkout
+curl https://mise.run | sh
+mise up
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-cargo install just
-
-mkdir -p "$HOME/tmp"
+just
