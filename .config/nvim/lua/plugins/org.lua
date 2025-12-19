6248dd20e7bf8b734e25a56ae62d2be68e7f0eb8
Paulo Suderio <paulo.suderio@gmail.com>
2025-12-19 16:36:08 -0300

    nvim org mode

---
+++ .config/nvim/lua/plugins/org.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "nvim-orgmode/orgmode",
+    event = "VeryLazy",
+    ft = { "org" },
+    config = function()
+      -- Setup orgmode
+      require("orgmode").setup({
+        org_agenda_files = "~/org/**/*",
+        org_default_notes_file = "~/org/refile.org",
+        org_todo_keywords = {
+          "TODO",
+          "WAITING",
+          "DOING",
+          "|",
+          "DONE(!)",
+          "CANCELLED(!)",
+        },
+        win_split_mode = "float",
+        win_border = "rounded",
+      })
+
+      -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+      -- add ~org~ to ignore_install
+      -- require('nvim-treesitter.configs').setup({
+      --   ensure_installed = 'all',
+      --   ignore_install = { 'org' },
+      -- })
+    end,
+  },
+}
