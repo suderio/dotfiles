6b63589a1bf0ee545b0142737e660652f236887e
Paulo Suderio <paulo.suderio@gmail.com>
2026-01-09 12:23:04 -0300

    Update dotfiles via script

--- .local/docker/centos-dev/start.sh
+++
@@ -1,7 +0,0 @@
-docker build -t centos-dev .
-
-docker run -it --rm \
-  -v "$HOME":"/home/$(id -un)" \
-  -e UID="$(id -u)" -e GID="$(id -g)" \
-  -e HOME="/home/$(id -un)" -e USERNAME="$(id -un)" \
-  centos-dev
