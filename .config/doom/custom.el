e1bacdaf831480157a8e287243f7a223f2e6bcb1
Paulo Suderio <paulo.suderio@gmail.com>
2026-01-26 19:59:19 -0300

    Update dotfiles via script

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -4,6 +4,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+      '("720838034f1dd3b3da66f6bd4d053ee67c93a747b219d1c546c41c4e425daf93"
+           "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8"
+           "f64189544da6f16bab285747d04a92bd57c7e7813d8c24c30f382f087d460a33"
+           "456697e914823ee45365b843c89fbc79191fdbaff471b29aad9dcbe0ee1d5641"
+           default))
  '(org-agenda-files
       '("/home/psude/org/inbox.org" "/home/psude/org/journal/202511.org")))
 (custom-set-faces
