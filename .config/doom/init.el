39fd0187358eab418fe98d14491ca9b0bdbe10d0
Paulo Suderio <paulo.suderio@gmail.com>
2026-03-03 15:44:39 -0300

    Update dotfiles via script

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -100,7 +100,7 @@
        (eval +overlay)
                                         ; run code, run (also, repls)
        (lookup +dictionary +offline)                         ; navigate your code and its documentation
-       ;;llm               ; when I said you needed friends, I didn't mean...
+       llm               ; when I said you needed friends, I didn't mean...
        (lsp +booster +eglot)                                 ; M-x vscode
        magit                               ; a git porcelain for Emacs
        make                                ; run make tasks from Emacs
